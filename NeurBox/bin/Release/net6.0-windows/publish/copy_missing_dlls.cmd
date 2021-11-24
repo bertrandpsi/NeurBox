6922a48890acc35a5ea6b5405d2c72fd2ac537b8
Alain Bertrand <alain.bertrand@psi.ch>
2021-11-24 13:49:42 +0100

    added script to copy missing dlls

---
+++ NeurBox/bin/Release/net6.0-windows/publish/copy_missing_dlls.cmd
@@ -0,0 +1,5 @@
+copy ..\win-x64\NeurBox.dll win-x64
+copy ..\win-x64\System.dll win-x64
+copy ..\win-x64\System.Core.dll win-x64
+copy ..\win-x64\System.Runtime.dll win-x64
+copy ..\win-x64\System.Private.CoreLib.dll win-x64
