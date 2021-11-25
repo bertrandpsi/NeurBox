244bbf72e4495e3d00cd94995e0171626ca8e6c2
Alain Bertrand <alain.bertrand@psi.ch>
2021-11-25 22:03:03 +0100

    .

---
+++ NeuroBox/bin/Release/net6.0-windows/publish/copy_missing_dlls.cmd
@@ -0,0 +1,7 @@
+@echo off
+echo Copy missing dlls
+copy ..\win-x64\NeuroBox.dll win-x64
+copy ..\win-x64\System.dll win-x64
+copy ..\win-x64\System.Core.dll win-x64
+copy ..\win-x64\System.Runtime.dll win-x64
+copy ..\win-x64\System.Private.CoreLib.dll win-x64
